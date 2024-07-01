674495d75a711ae63d0a762c57c52f40bb3bd0db
Schuyler Erle <schuyler@lattice.com>
2024-06-30 18:26:41 -0700

    Replace webpacker with esbuild

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root 'pages#home'
-  get '*path', to: 'pages#home'
+  #root 'pages#home'
+  #get '*path', to: 'pages#home'
 end
